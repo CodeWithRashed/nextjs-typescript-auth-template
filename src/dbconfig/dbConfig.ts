3f00236ec44d292a55873db15b01b21cd94183a9
Md Rashed Ali <tools.rashed@gmail.com>
2023-12-17 12:30:58 +0600

    database connection done

---
+++ src/dbconfig/dbConfig.ts
@@ -0,0 +1,21 @@
+import mongoose, { connection } from "mongoose";
+
+export async function connect() {
+    try{
+        mongoose.connect(process.env.DATABASE_URI!)
+
+        //On Successful Connection
+        connection.on("connected", ()=>{
+            console.log("Connected to Database")
+        })
+
+        //On Error Connection
+        connection.on("error", (err)=>{
+            console.log(err)
+            process.exit()
+        })
+
+    }catch{
+        console.log("error connecting database")
+    }
+}
